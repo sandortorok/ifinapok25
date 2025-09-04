6bcf4f3db3033f01666506e48b87f86c62438b0f
Török Sándor <sandort99@gmail.com>
2025-09-04 15:11:18 +0200

    a

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormComponent } from '../form/form.component';
 import { ProgramsComponent } from '../programs/programs.component';
+import { InformationComponent } from '../information/information.component';
 
 @Component({
   selector: 'app-main',
-  imports: [NavbarComponent, FormComponent, ProgramsComponent],
+  imports: [NavbarComponent, FormComponent, ProgramsComponent, InformationComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
